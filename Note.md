0cfe5545ca26ca0a7cadcc9e4ef96a9933e0c580
Valerie Alvarez <m15388108558_1@163.com>
2023-08-01 21:02:17 +0800

    Lesson 9 Day 3 Beginning with VRF

--- Note.md
+++ Note.md
@@ -1,4 +1,52 @@
 # Notes
-***
+
+---
+
 ## Good practice
-Do a "What-how" before hand, to help both developing and testing
+
+- Do a "What-how" before hand, to help both developing and testing
+- Test-oriented coding
+	- Write test first, code along
+
+---
+
+## Event
+
+-   EVM writes logs(to a special data structure) that can be read, events allow you to print stuff to this log
+    -   It is inaccessible for smart contract to read, this is why it's cheaper
+    -   Important for front-end
+    -   Chainlink relies on this to make API calls, provides random...
+-   Basic structure
+    > ```solidity
+    > event storedNumber(
+    > 	uint256 indexed oldNum,
+    > 	uint256 indexed newNum,
+    > 	uint256 addedNum,
+    > 	address sender
+    > );
+    > ```
+    -   Event of type `storedNumber`, when it's emitted, it will have these params
+    -   `indexed`
+        -   Can have up to 3 `indexed` params, known as `Topics`, these are searchable
+        -   Non-indexed are less searchable for being encoded in bytecode
+-   Emitting
+    > ```solidity
+    > emit storedNumber(
+    > 	favNum,
+    > 	_favNum,
+    > 	_favNum + favNum,
+    > 	msg.sender
+    > );
+    > ```
+- Structure of a log
+	- Address of the contract
+	- Name of the event
+	- Topics
+	- Data
+		- Non-indexed
+
+***
+
+## Chainlink VRF
+
+See [Doc](https://docs.chain.link/vrf/v2/subscription/examples/get-a-random-number)
--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 /**
  * What we want:
  *      People being able to buy-in
@@ -15,37 +18,73 @@ pragma solidity ^0.8.7;
 
 error Raffle__NotEnoughETH();
 
-contract Raffle {
-    uint256 private immutable i_entranceFee;
-    /** `address payable[]` initializes address[] that are payable(for the contract will have to pay the players) */
-    address payable[] private s_players;
+contract Raffle is VRFConsumerBaseV2 {
+	/** State variables */
+	uint256 private immutable i_entranceFee;
+	/** `address payable[]` initializes address[] that are payable(for the contract will have to pay the players) */
+	address payable[] private s_players;
+	VRFCoordinatorV2Interface vrfCoordinator;
 
-    constructor(uint256 entranceFee) {
-        i_entranceFee = entranceFee;
-    }
+	/** Events */
+	// Naming practice: reversed action name(enterRaffle)
+	event RaffleEnter(address indexed player);
 
-    function enterRaffle() public payable {
-        /**
-         * `msg.value` requires payable
-         */
-        if (msg.value < i_entranceFee) {
-            revert Raffle__NotEnoughETH();
-        }
-        s_players.push(payable(msg.sender));
+	/**
+	 * `VRFConsumerBaseV2` is the constructor for parent contract(class), also called main constructor
+	 */
+	constructor(
+		address vrfCoordinatorV2,
+		uint256 entranceFee
+	) VRFConsumerBaseV2(vrfCoordinatorV2) {
+		i_entranceFee = entranceFee;
+		vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2); // Timestamp 14:16:53
+	}
 
-        // Events
-        /**
-         * Whenever updating a dynamic object(arrays, mappings), we want to emit an event
-         */
-    }
+	function enterRaffle() public payable {
+		/**
+		 * `msg.value` requires payable
+		 */
+		if (msg.value < i_entranceFee) {
+			revert Raffle__NotEnoughETH();
+		}
+		s_players.push(payable(msg.sender));
 
-    // function pickRandomWinner() public returns (address) {}
+		/** Events
+		 *      Whenever updating a dynamic object(arrays, mappings), we want to emit an event, very important for front end
+		 */
+		emit RaffleEnter(msg.sender);
+	}
 
-    function getEntranceFee() public view returns (uint256) {
-        return i_entranceFee;
-    }
+	/**
+	 * Runs automatically
+	 * Visibility: `external` -> Cheaper and only available to outside this contract
+	 * 		See (https://docs.chain.link/vrf/v2/subscription/examples/get-a-random-number) src
+	 * 			`requestRandomWords` need to call VRFCoordinator, so we need this as well, imported and tracked above
+	 */
+	function requestRandomWinner() external {
+		/**
+		 * What to do:
+		 * 		1. Request the random num
+		 * 		2. Get(Fulfill) the random num
+		 * 		(Two txns, Reason for this: If only one txn, people can brute simulate calling the txn to try and manipulate the random num)
+		 */
+	}
 
-    function getPlayers(uint256 index) public view returns (address) {
-        return s_players[index];
-    }
+	/**
+	 * Will be overriding `fulfillRandomWords` from `VRFConsumerBaseV2`, so that VRFCoordinator knows that we can call `fulfillRandomWords`
+	 */
+	function fulfillRandomWords(
+		uint256 requestId,
+		uint256[] memory randomWords
+	) internal override {}
+
+	/** View / Pure functions */
+
+	function getEntranceFee() public view returns (uint256) {
+		return i_entranceFee;
+	}
+
+	function getPlayers(uint256 index) public view returns (address) {
+		return s_players[index];
+	}
 }
